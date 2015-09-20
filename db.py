7456b4f47b3fc5b3d1b36a08e7aeeb95cee5e2d7
Alexander Tsankov <antsankov@gmail.com>
2015-09-20 14:48:44 -0600

    worked in sam's changes, now for documentation

---
+++ db.py
@@ -0,0 +1,20 @@
+from remodel.models import Model
+from remodel.helpers import create_tables, create_indexes
+
+class Course(Model):
+    has_many = ('Section',)
+
+class Section(Model):
+    belongs_to = ('Course',)
+
+print("Hello world")
+create_tables()
+create_indexes()
+test_course = Course.create(name='MATH 1300')
+test_course['sections'].add(Section(name='8AM - DOE'), Section(name='2PM - CHANG'))
+
+def section_count():
+    return str(test_course['sections'].count()) # prints 2
+
+def course():
+    return str(test_course["name"])
--- server.py
+++ server.py
@@ -1,14 +1,30 @@
+import os.path
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
+from tornado import ioloop, gen
+from tornado.concurrent import Future, chain_future
+from tornado.options import define, options
 
-class MainHandler(tornado.web.RequestHandler):
+import db
+
+define("port", default=8000, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
-
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+        self.write(db.course() + ", SECTION COUNT: " + db.section_count())
 
 if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    tornado.options.parse_command_line()
+    # Tornado pro-tip: regex routing is optimized by putting more frequently
+    # accessed routes and simpler regexes before other routes.
+    app = tornado.web.Application(
+        handlers=[
+            (r"/", IndexHandler) #index
+        ]
+    )
+    static_path = os.path.join(os.path.dirname(__file__), "static"),
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
