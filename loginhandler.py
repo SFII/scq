a25b61a61464dcc3191fc086d7f8a3e13446e55c
untra <untra.sam@gmail.com>
2015-10-05 17:58:01 -0600

    lots of expansive changes. Some of these are organizational

--- loginhandler.py
+++ loginhandler.py
@@ -1,20 +1,32 @@
+import tornado.web
 import ldapauth
 import models/user
 
 class LoginHandler(tornado.web.RequestHandler):
+    LDAP_NAME = 'cn'
+    LDAP_MAIL = 'mail'
+    LDAP_MAJOR_1 = 'cuEduPersonPrimaryMajor1'
+    LDAP_MAJOR_2 = 'cuEduPersonPrimaryMajor2'
+    LDAP_MAJOR_3 = 'cuEduPersonSecondaryMajor1'
+    LDAP_MAJOR_4 = 'cuEduPersonSecondaryMajor2'
+    LDAP_MINOR_1 = 'cuEduPersonPrimaryMinor'
+    LDAP_MINOR_2 = 'cuEduPersonSecondaryMinor'
+    LDAP_STATUS = 'cuEduPersonClass'
+    LDAP_ATTRS = [LDAP_NAME,LDAP_MAJOR_1, LDAP_MAJOR_2, LDAP_MAJOR_3, LDAP_MAJOR_4, LDAP_MAIL, LDAP_MINOR_1, LDAP_MINOR_2, LDAP_STATUS]
+
     def get(self):
         loginForm()
 
     def post(self):
-        identikey = self.get_argument('identikey')
+        username = self.get_argument('username')
         password = self.get_argument('password')
-        authd = ldapauth.auth_user_ldap(identikey, password)
-        if authd:
-            attrs = ['cn','cuEduPersonPrimaryMajor1','cuEduPersonPrimaryMajor2','cuEduPersonSecondaryMajor1','cuEduPersonSecondaryMajor2','mail','cuEduPersonClass']
-            results = ldapauth.user_info_ldap(identikey, attrs)
-            writeout = "authd: {0}<br>".format(authd)
-            writeout += "{0}".format(results)
-            self.write(writeout)
+        authorized = ldapauth.auth_user_ldap(username, password)
+        if authorized:
+            user = getUser(username)
+            if user is None:
+                register(user)
+            else:
+                login(user)
         else:
             failToAuthenticate()
             loginForm()
@@ -27,16 +39,32 @@ class LoginHandler(tornado.web.RequestHandler):
         self.write("""
                 <form method="post" action="/myself">
                 <b>Enter Your Identikey</b><br>
-                <input type="text" name="identikey"><br>
+                <input type="text" name="username"><br>
                 <b>Enter Your Password</b><br>
                 <input type="password" name="password"></p>
                 <input type="submit" value="Submit">
                 </form>
             """)
 
-    def getUser(identikey):
-        user = User.get(uid=Identikey)
+    def register(user):
+        ldapinfo = ldapauth.user_info_ldap(username, LDAP_ATTRS)[0][1]
+        name = ldapinfo[LDAP_NAME][0]
+        mail = ldapinfo[LDAP_MAIL][0]
+        status = ldapinfo[LDAP_STATUS][0]
+        major1 = ldapinfo[LDAP_MAJOR_1][0] if LDAP_MAJOR_1 in ldapinfo else None
+        major2 = ldapinfo[LDAP_MAJOR_2][0] if LDAP_MAJOR_2 in ldapinfo else None
+        major3 = ldapinfo[LDAP_MAJOR_3][0] if LDAP_MAJOR_3 in ldapinfo else None
+        major4 = ldapinfo[LDAP_MAJOR_4][0] if LDAP_MAJOR_4 in ldapinfo else None
+        minor1 = ldapinfo[LDAP_MINOR_1][0] if LDAP_MINOR_1 in ldapinfo else None
+        minor2 = ldapinfo[LDAP_MINOR_2][0] if LDAP_MINOR_2 in ldapinfo else None
+        User.create
 
+    def login(user):
+        self.set_secure_cookie("username", self.get_argument("username"))
+        self.redirect("/")
 
-    def failToAuthenticate(identikey):
-        self.write("user and password not recognized.".format(identikey))
+    def getUser(username):
+        user = User.get(uid=username)
+
+    def failToAuthenticate(username):
+        self.write("user and password not recognized.".format(username))
--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from tornado.options import define, options
 
 import ldapauth
 import db
+from models.user import User
 
 define("port", default=8000, help="run on the given port", type=int)
 
@@ -31,9 +32,12 @@ class MyselfHandler(tornado.web.RequestHandler):
     def post(self):
         identikey = self.get_argument('identikey')
         password = self.get_argument('password')
+        print User
+        print User.get(uid=identikey)
         authd = ldapauth.auth_user_ldap(identikey, password)
-        attrs = ['cn','cuEduPersonPrimaryMajor1','cuEduPersonPrimaryMajor2','cuEduPersonSecondaryMajor1','cuEduPersonSecondaryMajor2','mail','cuEduPersonClass']
-        results = ldapauth.user_info_ldap(identikey, attrs)
+        attrs = ['cn','cuEduPersonPrimaryMajor1','cuEduPersonPrimaryMajor2','cuEduPersonSecondaryMajor1','cuEduPersonSecondaryMajor2','cuEduPersonSecondaryMinor','mail','cuEduPersonClass']
+        results = ldapauth.user_info_ldap(identikey, attrs)[0]
+        print results[1]
         writeout = "authd: {0}<br>".format(authd)
         if authd:
             writeout += "{0}".format(results)
@@ -42,6 +46,12 @@ class MyselfHandler(tornado.web.RequestHandler):
 
 def main():
     tornado.options.parse_command_line()
+    settings = {
+        'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+        'cookie_secret': '8goWPH9uTyO+9e2NzuaW6pbR6WKH1EbmrXIfxttXq00=',
+        'xsrf_cookies': True,
+        'login_url': '/login'
+    }
     # Tornado pro-tip: regex routing is optimized by putting more frequently
     # accessed routes and simpler regexes before other routes.
     app = tornado.web.Application(
