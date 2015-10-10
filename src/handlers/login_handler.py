bfba67935077238bfd5837fcab2f0f272fe6b38e
untra <untra.sam@gmail.com>
2015-10-10 17:56:32 -0600

    lots of progress. logout and login work like a charm. Now to make registration

--- src/handlers/login_handler.py
+++ src/handlers/login_handler.py
@@ -1,5 +1,6 @@
 import tornado.web
-from services.hashing import Hashing
+import logging
+from services import hashing
 from models.user import User
 
 class LoginHandler(tornado.web.RequestHandler):
@@ -11,38 +12,25 @@ class LoginHandler(tornado.web.RequestHandler):
         password = self.get_argument('password',strip = True)
         user = User.get(uid=username)
         if user is None:
-            self.render(
+            return self.render(
                 'login.html',
-                errormessage="User credentials are incorrect!",
+                errormessage="User credentials not found!",
                 next=self.get_argument("next","/")
             )
-        hashedpassword = password
-        if user['passwordhash_sha256'] ==
         else:
-            self.__set_current_user(user)
-            self.redirect(self.get_argument("next", "/"))
-
-
-    def __register(self, username):
-        ldapinfo = ldapauth.user_info_ldap(username, LDAP_ATTRS)[0][1]
-        name = ldapinfo[LDAP_NAME][0]
-        mail = ldapinfo[LDAP_MAIL][0]
-        status = ldapinfo[LDAP_STATUS][0]
-        major1 = ldapinfo[LDAP_MAJOR_1][0] if LDAP_MAJOR_1 in ldapinfo else None
-        major2 = ldapinfo[LDAP_MAJOR_2][0] if LDAP_MAJOR_2 in ldapinfo else None
-        major3 = ldapinfo[LDAP_MAJOR_3][0] if LDAP_MAJOR_3 in ldapinfo else None
-        major4 = ldapinfo[LDAP_MAJOR_4][0] if LDAP_MAJOR_4 in ldapinfo else None
-        minor1 = ldapinfo[LDAP_MINOR_1][0] if LDAP_MINOR_1 in ldapinfo else None
-        minor2 = ldapinfo[LDAP_MINOR_2][0] if LDAP_MINOR_2 in ldapinfo else None
-
+            hashedpassword = hashing.sha256(password)
+            if user['passwordhash'] == hashedpassword:
+                return __login(user)
+            else:
+                return self.render(
+                    'login.html',
+                    errormessage="User credentials are incorrect!",
+                    next=self.get_argument("next","/")
+                )
 
     def __login(self, user):
         self.__set_current_user(user)
-        self.set_secure_cookie("username", self.get_argument("username"))
-        self.redirect("/")
-
-    def __getUser(self, username):
-        user = User.get(uid=username)
+        self.redirect(self.get_argument("next", "/"))
 
     def __set_current_user(self, user):
         if user:
--- src/services/hashing.py
+++ src/services/hashing.py
@@ -1,8 +1,8 @@
 import hashlib
 
-class Hashing:
+
+def sha256(password):
+    # encoding = str.encode(password)
     hashing256 = hashlib.sha256()
-    def sha256(password):
-        encoding = str.encode(password)
-        hashing256.update(encoding)
-        hashing256.digest()
+    hashing256.update(password)
+    return hashing256.hexdigest()
