ad1dd4c0884068d371ab85025c05fb88875c9a21
sung <sungbae305@gmail.com>
2015-10-13 15:23:33 -0600

    initial db work

--- src/models/course.py
+++ src/models/course.py
@@ -1,3 +1,14 @@
 from remodel.models import Model
+from remodel.helpers import create_tables, create_indexes
+
 class Course(Model):
     has_many = ('Section',)
+
+def course(course):
+    return str(course['name'])
+
+
+# Creates all database tables defined by models
+create_tables()
+# Creates all table indexes based on model relations
+create_indexes()
--- src/models/section.py
+++ src/models/section.py
@@ -1,3 +1,14 @@
 from remodel.models import Model
+from remodel.helpers import create_tables, create_indexes
+
 class Section(Model):
     belongs_to = ('Course',)
+
+def section_count(section):
+    return str(section['sections'].count())
+
+# Creates all database tables defined by models
+create_tables()
+# Creates all table indexes based on model relations
+create_indexes()
+
