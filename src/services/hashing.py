8425ca40554700dd11c729d5c17cb4035f4c13e6
Nico Tonozzi <Nico Tonozzi>
2015-11-10 13:03:10 -0700

    Remove unused file

--- src/services/hashing.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def sha256(password):
-    # encoding = str.encode(password)
-    hashing256 = hashlib.sha256()
-    hashing256.update(password)
-    return hashing256.hexdigest()
