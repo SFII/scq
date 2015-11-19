6ba23d7abfd19a87c9621450b02a928aa8c9b6cc
Alexander Tsankov <antsankov@gmail.com>
2015-11-19 14:31:47 -0700

    Removed change

--- src/static/javascripts/db.js
+++ src/static/javascripts/db.js
@@ -16,6 +16,24 @@ function getSurvey(url){
     });
 }
 
-function hello(){
-    alert('hello')
+function postAnswer(){
+
 }
+
+getQuestion(){
+    return
+        {
+        "question-id" : "Unique-ID",
+        "type" : "multipleChoice",
+        "question":"Did you enjoy the course?",
+        "options":["Not at all","It was an average course","It was an excellent course"],
+        }
+}
+
+renderQuestion(question_object){
+    if question_object.type == "trueOrFalse" {
+        render
+    }
+}
+
+renderQuestion(getQuestion())
