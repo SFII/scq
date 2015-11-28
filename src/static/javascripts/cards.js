3d8f32aee4999cfcb53266275dcdb429e1f53d37
Alexander Tsankov <antsankov@gmail.com>
2015-11-28 13:51:15 -0500

    added in cards

--- src/static/javascripts/cards.js
+++ src/static/javascripts/cards.js
@@ -1,47 +1,37 @@
-// var MediumCard = React.createClass({
-//
-//     propTypes: {
-//         survey: React.PropTypes.object.isRequired
-//     },
-//
-//    render: function(){
-//       return(
-//          <div className="updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--8-col">
-//             <Card survey={ this.props.survey }/>
-//          </div>
-//       );
-//    }
-// });
-//
-// var SmallCard = React.createClass({
-//    render: function(){
-//       return(
-//          <div className="updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col">
-//             <Card options={ ['Small', 'TEST', 'SAMPLE THREE']}/>
-//          </div>
-//       );
-//    }
-// });
-//
-// var BigCard = React.createClass({
-//     render: function() {
-//         return (
-//             <div className="updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--12-col-desktop">
-//             <Card options={ ['SAMPLE ONE', 'SAMPLE TWO', 'SAMPLE THREE'] }/>
-//         </div>
-//         );
-//     }
-// });
-// var SupportSection = React.createClass({
-//
-//     render: function(){
-//       return (
-//         <div className="mdl-card__supporting-text mdl-color-text--grey-600">
-//             { this.props.survey }
-//         </div>
-//       );
-//     }
-// });
+var MediumCard = React.createClass({
+
+    propTypes: {
+        survey: React.PropTypes.object.isRequired
+    },
+
+   render: function(){
+      return(
+         <div className="updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--8-col">
+            <Card survey={ this.props.survey }/>
+         </div>
+      );
+   }
+});
+
+var SmallCard = React.createClass({
+   render: function(){
+      return(
+         <div className="updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col">
+            <Card options={ ['Small', 'TEST', 'SAMPLE THREE']}/>
+         </div>
+      );
+   }
+});
+
+var BigCard = React.createClass({
+    render: function() {
+        return (
+            <div className="updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--12-col-desktop">
+            <Card options={ ['SAMPLE ONE', 'SAMPLE TWO', 'SAMPLE THREE'] }/>
+        </div>
+        );
+    }
+});
 
 var TitleSection = React.createClass({
     render: function(){
