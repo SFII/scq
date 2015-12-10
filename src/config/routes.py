f9fd7cb5ee2fa0e6269f4f0a50515da1f6ac3608
sung <sungbae305@gmail.com>
2015-12-10 02:42:27 -0700

    Finished with getting/updating user info

--- src/config/routes.py
+++ src/config/routes.py
@@ -11,6 +11,8 @@ from handlers.register.culdap_register_handler import CuLdapRegisterHandler
 from handlers.dashboard_handler import DashboardHandler
 from handlers.survey_handler import Surveys
 from handlers.survey_handler import Response
+from handlers.user_info_handler import UserInfoHandler
+from handlers.user_info_update_handler import UserInfoUpdateHandler
 
 
 # Tornado pro-tip: regex routing is optimized by putting more frequently
@@ -23,5 +25,7 @@ routes = [
     (r"/register", RegisterHandler),
     (r"/dashboard",DashboardHandler),
     (r"/api/surveys", Surveys),
-    (r"/api/response", Response)
+    (r"/api/response", Response),
+    (r"/userinfo", UserInfoHandler),
+    (r"/userinfo/update", UserInfoUpdateHandler)
 ]
