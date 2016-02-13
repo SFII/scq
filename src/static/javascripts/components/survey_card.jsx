c1c92017d0b7c339ea43e2ef2fca1d39f7fd8329
Carlos Herrero Lozano <cahe5322@colorado.edu>
2016-02-13 14:43:55 -0700

    some react's magic in creating survey's card

--- src/static/javascripts/components/survey_card.jsx
+++ src/static/javascripts/components/survey_card.jsx
@@ -19,11 +19,15 @@ var SurveyCreationCard = React.createClass({
 
 var Fields = React.createClass({
 
-    changeHandler: function() {
-        //change handler, modifies the card
-        //alert("hello");
+    //set initial value
+    getInitialState: function() {
+        return {value: 'select'};
     },
 
+    //set value change
+    changeHandler: function(event) {
+        this.setState({value: event.target.value});
+    },
 
     render: function(){
       return (
@@ -32,16 +36,54 @@ var Fields = React.createClass({
               <form>
                   <p>Title:<input type="text" name="title_question"/></p>
                   <p>Type:
-                     <select id="select" onChange={this.changeHandler}>
-                         <option value="MultipleChoice">Multiple Choice</option>
-                         <option value="SingleChoice">Single Choice</option>
-                         <option value="Rating">Rating</option>
-                         <option value="FreeResponse">Open Response</option>
+                     <select id="mySelect" onChange={this.changeHandler} value={this.state.value}>
+                         <option disabled value="select"> Select an option </option>
+                         <option value="multipleChoice">Multiple Choice</option>
+                         <option value="singleChoice">Single Choice</option>
+                         <option value="rating">Rating</option>
+                         <option value="freeResponse">Open Response</option>
                      </select>
                   </p>
               </form>
-
+                  <Question value={this.state.value}/>
           </div>
       );
+    }
+});
+
+var Question = React.createClass({
+
+    changeHandler: function(event) {
+        //not sure if needed
+        this.setState({value: event.target.value});
+    },
+
+    render: function(){
+
+        if(this.props.value == "multipleChoice"){
+          return (
+          <input type="text" value={this.props.value} onChange={this.changeHandler}/>
+
+
+        } else if(this.props.value == "singleChoice"){
+          return (
+            <input type="text" value={this.props.value} onChange={this.changeHandler}/>
+          );
+
+        } else if(this.props.value == "rating"){
+          return (
+            <input type="text" value={this.props.value} onChange={this.changeHandler}/>
+          );
+
+        } else if(this.props.value == "freeResponse"){
+          return (
+            <input type="text" value={this.props.value} onChange={this.changeHandler}/>
+          );
+
+        } else {
+        return (
+        <p onChange={this.changeHandler}></p>
+        );
+        }
     }
 });
