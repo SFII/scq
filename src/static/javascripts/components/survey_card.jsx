b049c9dc1e2861448bde4b20d565b915a89c1582
Carlos Herrero Lozano <cahe5322@colorado.edu>
2016-02-15 16:26:05 -0700

    small changes before Sam's big pr

--- src/static/javascripts/components/survey_card.jsx
+++ src/static/javascripts/components/survey_card.jsx
@@ -53,24 +53,53 @@ var Fields = React.createClass({
 
 var Question = React.createClass({
 
+    //mdl in new questions
+    componentDidUpdate: function(){
+        componentHandler.upgradeDom();
+    },
+
     changeHandler: function(event) {
         //not sure if needed
         this.setState({value: event.target.value});
     },
 
+    //render different options for multiple & single choice
+    /*
+    *
+    render: function(){
+      const options = this.props.options.map((option) => {
+        return (
+          <li className="mdl-list__item">
+            <input  type="text"  placeholder="Introduce an option" onChange={this.changeHandler}/>
+          </li>
+        )
+      });
+    *
+    */
+
     render: function(){
 
         if(this.props.value == "multipleChoice"){
           return (
-              <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-                <input
-                  type="checkbox"
-                  name = "option1"
-                  value = "option1"
-                  className="mdl-checkbox__input"
-                  />
-              <input className="mdl-checkbox__label" type="text" value={this.props.value} onChange={this.changeHandler}/>
-          </label>
+            <div>
+            <ul className="mdl-list">
+               {options}
+              </ul>
+
+              <ul className="no_bullets mdl-list">
+                <li className="mdl-list__item">
+                <p>
+                  <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
+                  <i className="material-icons">add</i>
+
+                  </button>
+
+                &nbsp;&nbsp;&nbsp; ADD QUESTION
+                </p>
+               </li>
+
+            </ul>
+            </div>
           );
 
         } else if(this.props.value == "singleChoice"){
