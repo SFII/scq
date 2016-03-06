794f72848668e35fe881a2805b9fbd8c2d82f233
sungbae <sungbae305@gmail.com>
2016-03-06 15:55:46 -0700

    added all my work from sung_profile_v1 to v2

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -1,5 +1,7 @@
 import tornado.web
 from handlers.base_handler import BaseHandler
+from models.course import Course
+from models.user import User
 import logging
 
 
@@ -8,8 +10,24 @@ class ProfileHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         self.refresh_current_user_cookie()
-        self.render('profile.html', user_info_json=self.get_user_info())
-    
+        self.render('profile.html', extra_info_json=self.get_extra_info(), user_info_json=self.get_user_info())
+
+    def post(self):
+        pass
+
+    def get_extra_info(self):
+        extra_info_json = []
+        extra_dict = {
+            'gender': User.USER_GENDERS,
+            'primary_affiliation': User.USER_PRIMARY_AFFILIATION,
+            'ethnicity': User.USER_ETHNICITIES,
+            'native_language': User.USER_NATIVE_LANGUAGES,
+            'status': User.USER_STATUS
+        }
+        extra_info_json.append(extra_dict)
+        logging.info("SUNG LOOK {0}".format(extra_info_json))
+        return tornado.escape.json_encode(extra_info_json)
+
     def get_user_info(self):
         user_info_json = []
         user_data = self.current_user
--- static/javascripts/components/profile.jsx
+++ static/javascripts/components/profile.jsx
@@ -1,90 +1,87 @@
 var ProfilePage = React.createClass({
-getInitialState: function(){
-    var departments = '';
-    var courses = '';
-    var courses_taught = '';
-    var status = '';
-    
-    
-    if(user_data[0].departments.length == 0 || user_data[0].departments == ['']){
-        console.log('default');
-        departments = 'Not enrolled into any departments';
+  getInitialState: function(){
+    var user_groups = '';
+    var user_status = '';
+    var num_sub_groups = user_data[0].subscribed_groups.length;
+    if ((num_sub_groups == 0) || (user_data[0].subscribed_groups == [''])) {
+      user_groups = 'Not subscribed into any groups';
+    } else {
+      for (var i = 0; i < num_sub_groups; i++) {
+        user_groups += user_data[0].subscribed_groups[i];
+        if (i < num_sub_groups - 1) {
+          user_groups += "\n";
+        }
+      }
     }
-    else{
-        for(var i = 0; i < user_data[0].departments.length; i++){
-            departments += user_data[0].departments[i];
-            if(i < user_data[0].departments.length-1){
-                console.log("new line");
-                departments += "\n";
-            }
-            console.log("Departments state: " + departments);
-        }
+    if (user_data[0].primary_affiliation == "Student") {
+      if (user_data[0].status) {
+        user_status = user_data[0].status;
+      } else {
+        user_status = "Did not specify academic year";
+      }
     }
-    
-    if(user_data[0].primary_affiliation == "Student"){
-        if(user_data[0].status){
-            status = user_data[0].status;
-        }
-        else{
-            status = "Did not specify academic year";
-        }
-        if(user_data[0].courses.length == 0 || user_data[0].courses == ['']){
-            courses = 'Not enrolled into any courses';
-        }
-        else{
-            for(var i = 0; i < user_data[0].courses.length; i++){
-                courses += user_data[0].courses[i];
-                if(i < user_data[0].courses.length-1){
-                    console.log("new line");
-                    courses += "\n";
-                }
-                console.log("Courses state: " + courses);
-            }
-        }
-    }
-    
-    else if(user_data[0].primary_affiliation[0] == "Faculty"){
-        if(user_data[0].courses_taught.length == 0 || user_data[0].courses_taught == ['']){
-            courses_taught = 'Not enrolled into any departments';
-        }
-        else{
-            for(var i = 0; i < user_data[0].courses_taught.length; i++){
-                courses_taught += user_data[0].courses_taught[i];
-                if(i < user_data[0].courses_taught.length-1){
-                    courses_taught += "\n";
-                }
-                console.log("Courses taught state: " + courses_taught);
-            }
-        }
-    }
-    return{
-    departments: departments,
-    courses: courses,
-    courses_taught: courses_taught,
-    status: status,
-    }
-},
+    return {
+      username: user_data[0].username,
+      user_affiliation: user_data[0].primary_affiliation,
+      email: user_data[0].email,
+      user_gender: user_data[0].gender,
+      user_ethnicity: user_data[0].ethnicity,
+      user_native_language: user_data[0].native_language,
+      user_status: user_data[0].status,
+      user_groups: user_groups,
+      affiliation: extra_data[0].primary_affiliation,
+      gender: extra_data[0].gender,
+      ethnicity: extra_data[0].ethnicity,
+      native_language: extra_data[0].native_language,
+      status: extra_data[0].status,
 
-    render: function(){
-    console.log(user_data)
+    };
+  },
+  _onClick: function() {
+    var message = "hello world";
+    this.setState({message});
+  },
+  handleChange: function(key) {
+    return function (e) {
+      var state = {};
+      state[key] = e.target.value;
+      this.setState(state);
+    }.bind(this);
+  },
+  render: function(){
+    var style = {
+      listStyleType: "none",
+      fontSize: "20px"
+    };
     return(
-        <div className="mdl-card mdl-shadow--2dp">
-            <div className="mdl-card__title mdl-color--primary">
-                <h2 className="mdl-card__title-text">User Info</h2>
-            </div>
-            <div className="mdl-card__supporting-text">
-            Username: {user_data[0].username} <br/>
-            User Type: {user_data[0].primary_affiliation} <br/>
-            Email: {user_data[0].email} <br/>
-            Birth Date: {user_data[0].dob} <br/>
-            Gender: {user_data[0].gender} <br/>
-            Ethnicity: {user_data[0].ethnicity} <br/>
-            Native Language: {user_data[0].native_language} <br/>
-            Status: {this.state.status} <br/>
-            Courses Enrolled: {this.state.courses} <br/>
-            Departments: {this.state.departments} <br/>
-            </div>
-        </div>
-    );
+      <ul style={style}>
+      Username: <input value={this.state.username} onChange={this.handleChange('username')} /><br/>
+      Affiliation(s): <select>
+                        <option value={this.state.user_affiliation}>{this.state.user_affiliation}</option>
+                        <option value={this.state.affiliation}>{this.state.affiliation}</option>
+                      </select><br/>
+      Email: <input value={this.state.email} onChange={this.handleChange('email')} /><br/>
+      Birth Date: <input type="text" value={user_data[0].dob}/><br/>
+      Gender: <select>
+                <option value={this.state.user_gender}>{this.state.user_gender}</option>
+                <option value={this.state.gender}>{this.state.gender}</option>
+              </select><br/>
+      Ethnicity: <select>
+                   <option value={this.state.user_ethnicity}>{this.state.user_ethnicity}</option>
+                   <option value={this.state.ethnicity}>{this.state.ethnicity}</option>
+                 </select><br/>
+      Native Language: <select>
+                         <option value={this.state.user_native_language}>{this.state.user_native_language}</option>
+                         <option value={this.state.native_language}>{this.state.native_language}</option>
+                       </select><br/>
+      Academic Year: <select>
+                       <option value={this.state.user_status}>{this.state.user_status}</option>
+                       <option value={this.state.status}>{this.state.status}</option>
+                     </select><br/>
+      Group(s) Subscribed: <textarea name="courses" cols="30" rows="5" value={this.state.user_groups} /><br/>
+      <br/>
+      <button onClick={this._onClick} value="Change Message">{this.state.message}</button>
+      </ul>
+      );
     }
 });
