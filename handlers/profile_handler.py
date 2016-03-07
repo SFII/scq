92b428b0966ae06021de1ccb72b4142bf538decd
sungbae <sungbae305@gmail.com>
2016-03-06 20:02:42 -0700

    add submit button

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -13,7 +13,27 @@ class ProfileHandler(BaseHandler):
         self.render('profile.html', extra_info_json=self.get_extra_info(), user_info_json=self.get_user_info())
 
     def post(self):
-        pass
+        data = User().default()
+        data['username'] = self.current_user['username']
+        data['email'] = self.get_argument('email', None, strip=True)
+        data['dob'] = self.get_argument('dob', None, strip=True)
+        data['gender'] = self.get_argument('gender', None, strip=True)
+        data['ethnicity'] = self.get_argument('ethnicity', None, strip=True)
+        data['native_language'] = self.get_argument('native_language', None, strip=True)
+        data['status'] = self.get_argument('status', '', strip=True)
+        data['primary_affiliation'] = (self.get_argument('primary_affiliation', None, strip=True))
+        data['subscribed_groups'] = self.get_argument('subscribed_groups', None, strip=True)
+        if (data['subscribed_groups'] is None):
+            data['courses'] = []
+        else:
+            data['subscribed_groups'] = (self.get_argument('subscribed_groups', None, strip=True)).replace(' ', '').split(',')
+        verified = User().verify(data)
+        if len(verified) != 0:
+            logging.error('User: verification errors in user update page!')
+            logging.error(verified)
+            return self.redirect(self.get_argument("/profile"))
+        User().update_item(self.current_user['id'], data)
+        return self.redirect(self.get_argument("next", "/dashboard"))
 
     def get_extra_info(self):
         user_data = self.current_user
@@ -25,11 +45,15 @@ class ProfileHandler(BaseHandler):
             'native_language': User.USER_NATIVE_LANGUAGES,
             'status': User.USER_STATUS
         }
-        if (len(extra_dict['gender']) == 2):
+        if (user_data['gender'] in extra_dict['gender']):
             extra_dict['gender'].remove(user_data['gender'])
+        if (user_data['primary_affiliation'] in extra_dict['primary_affiliation']):
             extra_dict['primary_affiliation'].remove(user_data['primary_affiliation'])
+        if (user_data['ethnicity'] in extra_dict['ethnicity']):
             extra_dict['ethnicity'].remove(user_data['ethnicity'])
+        if (user_data['native_language'] in extra_dict['native_language']):
             extra_dict['native_language'].remove(user_data['native_language'])
+        if (user_data['status'] in extra_dict['status']):
             extra_dict['status'].remove(user_data['status'])
         extra_info_json.append(extra_dict)
         return tornado.escape.json_encode(extra_info_json)
--- static/javascripts/components/profile.jsx
+++ static/javascripts/components/profile.jsx
@@ -1,15 +1,15 @@
 var ProfilePage = React.createClass({
   getInitialState: function(){
-    var user_groups = '';
+    var subscribed_groups = '';
     var user_status = '';
     var num_sub_groups = user_data[0].subscribed_groups.length;
     if ((num_sub_groups == 0) || (user_data[0].subscribed_groups == [''])) {
-      user_groups = '';
+      subscribed_groups = '';
     } else {
       for (var i = 0; i < num_sub_groups; i++) {
-        user_groups += user_data[0].subscribed_groups[i];
+        subscribed_groups += user_data[0].subscribed_groups[i];
         if (i < num_sub_groups - 1) {
-          user_groups += "\n";
+          subscribed_groups += "\n";
         }
       }
     }
@@ -29,7 +29,7 @@ var ProfilePage = React.createClass({
       user_ethnicity: user_data[0].ethnicity,
       user_native_language: user_data[0].native_language,
       user_status: user_data[0].status,
-      user_groups: user_groups,
+      subscribed_groups: subscribed_groups,
       affiliation: extra_data[0].primary_affiliation,
       gender: extra_data[0].gender,
       ethnicity: extra_data[0].ethnicity,
@@ -37,10 +37,6 @@ var ProfilePage = React.createClass({
       status: extra_data[0].status,
 
     };
-  },
-  _onClick: function() {
-    var message = "hello world";
-    this.setState({message});
   },
   handleChange: function(key) {
     return function (e) {
@@ -56,53 +52,56 @@ var ProfilePage = React.createClass({
     };
     return(
       <ul style={style}>
-      Username: <input value={this.state.username} readOnly /><br/>
-      Affiliation(s): <select>
-                        <option value={this.state.user_affiliation}>{this.state.user_affiliation}</option>
-                        <option value={this.state.affiliation}>{this.state.affiliation[0]}</option>
-                        <option value={this.state.affiliation}>{this.state.affiliation[1]}</option>
-                      </select><br/>
-      Email: <input value={this.state.email} onChange={this.handleChange('email')} /><br/>
-      Birth Date: <input type="date" value={this.state.dob} onChange={this.handleChange('dob')} /><br/>
-      Gender: <select>
-                <option value={this.state.user_gender}>{this.state.user_gender}</option>
-                <option value={this.state.gender}>{this.state.gender[0]}</option>
-              </select><br/>
-      Ethnicity: <select>
-                   <option value={this.state.user_ethnicity}>{this.state.user_ethnicity}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[0]}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[1]}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[2]}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[3]}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[4]}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[5]}</option>
-                   <option value={this.state.ethnicity}>{this.state.ethnicity[6]}</option>
-                 </select><br/>
-      Native Language: <select>
-                         <option value={this.state.user_native_language}>{this.state.user_native_language}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[0]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[1]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[2]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[3]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[4]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[5]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[6]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[7]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[8]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[9]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[10]}</option>
-                         <option value={this.state.native_language}>{this.state.native_language[11]}</option>
+      <form action="/profile" method="post">
+        Username: <input value={this.state.username} readOnly /><br/>
+        Affiliation(s): <select>
+                          <option value={this.state.user_affiliation}>{this.state.user_affiliation}</option>
+                          <option value={this.state.affiliation}>{this.state.affiliation[0]}</option>
+                          <option value={this.state.affiliation}>{this.state.affiliation[1]}</option>
+                        </select><br/>
+        Email: <input value={this.state.email} onChange={this.handleChange('email')} required /><br/>
+        Birth Date: <input type="date" value={this.state.dob} onChange={this.handleChange('dob')} required /><br/>
+        Gender: <select>
+                  <option value={this.state.user_gender}>{this.state.user_gender}</option>
+                  <option value={this.state.gender}>{this.state.gender[0]}</option>
+                </select><br/>
+        Ethnicity: <select>
+                     <option value={this.state.user_ethnicity}>{this.state.user_ethnicity}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[0]}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[1]}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[2]}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[3]}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[4]}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[5]}</option>
+                     <option value={this.state.ethnicity}>{this.state.ethnicity[6]}</option>
+                   </select><br/>
+        Native Language: <select>
+                           <option value={this.state.user_native_language}>{this.state.user_native_language}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[0]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[1]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[2]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[3]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[4]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[5]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[6]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[7]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[8]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[9]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[10]}</option>
+                           <option value={this.state.native_language}>{this.state.native_language[11]}</option>
+                         </select><br/>
+        Academic Year: <select>
+                         <option value={this.state.user_status}>{this.state.user_status}</option>
+                         <option value={this.state.status}>{this.state.status[0]}</option>
+                         <option value={this.state.status}>{this.state.status[1]}</option>
+                         <option value={this.state.status}>{this.state.status[2]}</option>
                        </select><br/>
-      Academic Year: <select>
-                       <option value={this.state.user_status}>{this.state.user_status}</option>
-                       <option value={this.state.status}>{this.state.status[0]}</option>
-                       <option value={this.state.status}>{this.state.status[1]}</option>
-                       <option value={this.state.status}>{this.state.status[2]}</option>
-                     </select><br/>
-      Group(s) Subscribed: <br/><textarea name="courses" cols="30" rows="5" value={this.state.user_groups} onChange={this.handleChange('user_groups')} /><br/>
-      <br/>
-      <button onClick={this._onClick} value="Change Message">{this.state.message}</button>
+        Group(s) Subscribed: <br/><textarea name="courses" cols="30" rows="5" value={this.state.subscribed_groups}
+                                    onChange={this.handleChange('subscribed_groups')} /><br/>
+        <br/>
+        <input type="submit" value="submit" />
+      </form>
       </ul>
-      );
-    }
+    );
+  }
 });
