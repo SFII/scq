e5dff53ae71a2dc2bbdba9202bd082c34903253c
sungbae <sungbae305@gmail.com>
2016-03-06 21:35:10 -0700

    Finished POST profile

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -1,5 +1,5 @@
 import tornado.web
-from handlers.base_handler import BaseHandler
+from handlers.base_handler import BaseHandler, refresh_user_cookie_callback
 from models.course import Course
 from models.user import User
 import logging
@@ -7,12 +7,31 @@ import logging
 
 class ProfileHandler(BaseHandler):
 
+    extra_dict = {
+        'gender': User.USER_GENDERS,
+        'primary_affiliation': User.USER_PRIMARY_AFFILIATION,
+        'ethnicity': User.USER_ETHNICITIES,
+        'native_language': User.USER_NATIVE_LANGUAGES,
+        'status': User.USER_STATUS
+    }
+
     @tornado.web.authenticated
     def get(self):
         self.refresh_current_user_cookie()
         self.render('profile.html', extra_info_json=self.get_extra_info(), user_info_json=self.get_user_info())
 
     def post(self):
+        user_data = self.current_user
+        if (user_data['gender'] not in self.extra_dict['gender']):
+            self.extra_dict['gender'].append(user_data['gender'])
+        if (user_data['primary_affiliation'] not in self.extra_dict['primary_affiliation']):
+            self.extra_dict['primary_affiliation'].append(user_data['primary_affiliation'])
+        if (user_data['ethnicity'] not in self.extra_dict['ethnicity']):
+            self.extra_dict['ethnicity'].append(user_data['ethnicity'])
+        if (user_data['native_language'] not in self.extra_dict['native_language']):
+            self.extra_dict['native_language'].append(user_data['native_language'])
+        if (user_data['status'] not in self.extra_dict['status']):
+            self.extra_dict['status'].append(user_data['status'])
         data = User().default()
         data['username'] = self.current_user['username']
         data['email'] = self.get_argument('email', None, strip=True)
@@ -24,38 +43,32 @@ class ProfileHandler(BaseHandler):
         data['primary_affiliation'] = (self.get_argument('primary_affiliation', None, strip=True))
         data['subscribed_groups'] = self.get_argument('subscribed_groups', None, strip=True)
         if (data['subscribed_groups'] is None):
-            data['courses'] = []
+            data['subscribed_groups'] = []
         else:
             data['subscribed_groups'] = (self.get_argument('subscribed_groups', None, strip=True)).replace(' ', '').split(',')
         verified = User().verify(data)
         if len(verified) != 0:
-            logging.error('User: verification errors in user update page!')
+            logging.error('User: verification errors in POST profile page!')
             logging.error(verified)
-            return self.redirect(self.get_argument("/profile"))
+            return self.redirect(self.get_argument("next", "/dashboard"))
         User().update_item(self.current_user['id'], data)
-        return self.redirect(self.get_argument("next", "/dashboard"))
+        self.refresh_current_user_cookie()
+        return self.redirect(self.get_argument("next", "/profile"))
 
     def get_extra_info(self):
         user_data = self.current_user
         extra_info_json = []
-        extra_dict = {
-            'gender': User.USER_GENDERS,
-            'primary_affiliation': User.USER_PRIMARY_AFFILIATION,
-            'ethnicity': User.USER_ETHNICITIES,
-            'native_language': User.USER_NATIVE_LANGUAGES,
-            'status': User.USER_STATUS
-        }
-        if (user_data['gender'] in extra_dict['gender']):
-            extra_dict['gender'].remove(user_data['gender'])
-        if (user_data['primary_affiliation'] in extra_dict['primary_affiliation']):
-            extra_dict['primary_affiliation'].remove(user_data['primary_affiliation'])
-        if (user_data['ethnicity'] in extra_dict['ethnicity']):
-            extra_dict['ethnicity'].remove(user_data['ethnicity'])
-        if (user_data['native_language'] in extra_dict['native_language']):
-            extra_dict['native_language'].remove(user_data['native_language'])
-        if (user_data['status'] in extra_dict['status']):
-            extra_dict['status'].remove(user_data['status'])
-        extra_info_json.append(extra_dict)
+        if (user_data['gender'] in self.extra_dict['gender']):
+            self.extra_dict['gender'].remove(user_data['gender'])
+        if (user_data['primary_affiliation'] in self.extra_dict['primary_affiliation']):
+            self.extra_dict['primary_affiliation'].remove(user_data['primary_affiliation'])
+        if (user_data['ethnicity'] in self.extra_dict['ethnicity']):
+            self.extra_dict['ethnicity'].remove(user_data['ethnicity'])
+        if (user_data['native_language'] in self.extra_dict['native_language']):
+            self.extra_dict['native_language'].remove(user_data['native_language'])
+        if (user_data['status'] in self.extra_dict['status']):
+            self.extra_dict['status'].remove(user_data['status'])
+        extra_info_json.append(self.extra_dict)
         return tornado.escape.json_encode(extra_info_json)
 
     def get_user_info(self):
--- static/javascripts/components/profile.jsx
+++ static/javascripts/components/profile.jsx
@@ -53,50 +53,52 @@ var ProfilePage = React.createClass({
     return(
       <ul style={style}>
       <form action="/profile" method="post">
-        Username: <input value={this.state.username} readOnly /><br/>
-        Affiliation(s): <select>
+        Username: <input name="username" value={this.state.username} readOnly /><br/>
+        Affiliation(s): <select name="primary_affiliation">
                           <option value={this.state.user_affiliation}>{this.state.user_affiliation}</option>
-                          <option value={this.state.affiliation}>{this.state.affiliation[0]}</option>
-                          <option value={this.state.affiliation}>{this.state.affiliation[1]}</option>
+                          <option value={this.state.affiliation[0]}>{this.state.affiliation[0]}</option>
+                          <option value={this.state.affiliation[1]}>{this.state.affiliation[1]}</option>
                         </select><br/>
-        Email: <input value={this.state.email} onChange={this.handleChange('email')} required /><br/>
-        Birth Date: <input type="date" value={this.state.dob} onChange={this.handleChange('dob')} required /><br/>
-        Gender: <select>
+        Email: <input type="email" name="email" value={this.state.email} onChange={this.handleChange('email')} required /><br/>
+        Birth Date: <input type="date" name="dob" value={this.state.dob} onChange={this.handleChange('dob')} required /><br/>
+        Gender: <select name="gender">
                   <option value={this.state.user_gender}>{this.state.user_gender}</option>
-                  <option value={this.state.gender}>{this.state.gender[0]}</option>
+                  <option value={this.state.gender[0]}>{this.state.gender[0]}</option>
+                  <option value={this.state.gender[1]}>{this.state.gender[1]}</option>
+                  <option value={this.state.gender[2]}>{this.state.gender[2]}</option>
                 </select><br/>
-        Ethnicity: <select>
+        Ethnicity: <select name="ethnicity">
                      <option value={this.state.user_ethnicity}>{this.state.user_ethnicity}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[0]}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[1]}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[2]}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[3]}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[4]}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[5]}</option>
-                     <option value={this.state.ethnicity}>{this.state.ethnicity[6]}</option>
+                     <option value={this.state.ethnicity[0]}>{this.state.ethnicity[0]}</option>
+                     <option value={this.state.ethnicity[1]}>{this.state.ethnicity[1]}</option>
+                     <option value={this.state.ethnicity[2]}>{this.state.ethnicity[2]}</option>
+                     <option value={this.state.ethnicity[3]}>{this.state.ethnicity[3]}</option>
+                     <option value={this.state.ethnicity[4]}>{this.state.ethnicity[4]}</option>
+                     <option value={this.state.ethnicity[5]}>{this.state.ethnicity[5]}</option>
+                     <option value={this.state.ethnicity[6]}>{this.state.ethnicity[6]}</option>
                    </select><br/>
-        Native Language: <select>
+        Native Language: <select name="native_language">
                            <option value={this.state.user_native_language}>{this.state.user_native_language}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[0]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[1]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[2]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[3]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[4]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[5]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[6]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[7]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[8]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[9]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[10]}</option>
-                           <option value={this.state.native_language}>{this.state.native_language[11]}</option>
+                           <option value={this.state.native_language[0]}>{this.state.native_language[0]}</option>
+                           <option value={this.state.native_language[1]}>{this.state.native_language[1]}</option>
+                           <option value={this.state.native_language[2]}>{this.state.native_language[2]}</option>
+                           <option value={this.state.native_language[3]}>{this.state.native_language[3]}</option>
+                           <option value={this.state.native_language[4]}>{this.state.native_language[4]}</option>
+                           <option value={this.state.native_language[5]}>{this.state.native_language[5]}</option>
+                           <option value={this.state.native_language[6]}>{this.state.native_language[6]}</option>
+                           <option value={this.state.native_language[7]}>{this.state.native_language[7]}</option>
+                           <option value={this.state.native_language[8]}>{this.state.native_language[8]}</option>
+                           <option value={this.state.native_language[9]}>{this.state.native_language[9]}</option>
+                           <option value={this.state.native_language[10]}>{this.state.native_language[10]}</option>
+                           <option value={this.state.native_language[11]}>{this.state.native_language[11]}</option>
                          </select><br/>
-        Academic Year: <select>
+        Academic Year: <select name="status">
                          <option value={this.state.user_status}>{this.state.user_status}</option>
-                         <option value={this.state.status}>{this.state.status[0]}</option>
-                         <option value={this.state.status}>{this.state.status[1]}</option>
-                         <option value={this.state.status}>{this.state.status[2]}</option>
+                         <option value={this.state.status[0]}>{this.state.status[0]}</option>
+                         <option value={this.state.status[1]}>{this.state.status[1]}</option>
+                         <option value={this.state.status[2]}>{this.state.status[2]}</option>
                        </select><br/>
-        Group(s) Subscribed: <br/><textarea name="courses" cols="30" rows="5" value={this.state.subscribed_groups}
+        Group(s) Subscribed: <br/><textarea name="subscribed_groups" cols="30" rows="5" value={this.state.subscribed_groups}
                                     onChange={this.handleChange('subscribed_groups')} /><br/>
         <br/>
         <input type="submit" value="submit" />
