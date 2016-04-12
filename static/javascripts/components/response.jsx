00efa9b6d34bdbdd7bdc7794f28f98272e870cdd
Bradyta <brady.auen@colorado.edu>
2016-04-12 13:23:11 -0600

    ratings data and chartist import

--- static/javascripts/components/response.jsx
+++ static/javascripts/components/response.jsx
@@ -11,6 +11,7 @@ var ResponseCard = React.createClass({
             type: 'GET',
             success: function(results){
                 this.setState({results: results})
+                console.log(this.state.results);
             }.bind(this),
             error: function(xhr, status, err){
                 console.error("/api/results", status, err.toString());
@@ -19,9 +20,37 @@ var ResponseCard = React.createClass({
     },
 
     render: function(){
+    //map each object in results array to a div, we need different charts d
+    //depending on the question type
+    console.log(Object.prototype.toString.call(this.state.results));
+    var questionNodes = this.state.results.map(function(item) {
+        if(item.response_format == "rating"){
+            var data = {
+                series : item.bar_data.series,
+                labels : item.bar_data.labels
+            };
+            var options= {
+                seriesBarDistance: 15
+            };
+            new Chartist.Bar('.chart',data, options)
+        }
+        //pie data is represented as the set of options chosen with a value indicating
+        //how many times that set is chosen.
+        else if(item.response_format == "trueOrFalse"){
+
+        }
+        else if(item.response_format == "multipleChoice"){
+
+        }
+        else if(item.response_format == "freeResponse"){
+
+        }
+
+    });
+    
+    
         return(
-            <div className="updates mdl-card">
-                {JSON.stringify(this.state.results)}
+            <div className="updates chart mdl-card">
             </div>
         );
     },
