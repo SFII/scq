80c79464438a123aaa974fd2f253016d6504f994
Bradyta <brady.auen@colorado.edu>
2016-04-13 17:07:40 -0600

    fuck charts

--- static/javascripts/components/response.jsx
+++ static/javascripts/components/response.jsx
@@ -2,8 +2,7 @@ var ResponseCard = React.createClass({
 
     getInitialState: function() {
         return({
-            results: [7],
-            iter: 3
+            results: []
         })
     },
 
@@ -15,7 +14,6 @@ var ResponseCard = React.createClass({
             success: function(results){
                 this.setState({results: results})
                 console.log(this.state.results);
-                console.log(this.state.results[this.state.iter]);
             }.bind(this),
             error: function(xhr, status, err){
                 console.error("/api/results", status, err.toString());
@@ -24,49 +22,21 @@ var ResponseCard = React.createClass({
     },
 
     render: function(){
-    if(this.state.results[this.state.iter] != null){
+    var questionNodes = this.state.results.map(function(item) {
+    if(item.response_format == "rating"){
+        var data = {
+            series: item.bar_data.series,
+            labels: item.bar_data.labels
+        };
+        var options = {
+            seriesBarDistance: 15
+        };
+        new Chartist.Bar('.chart',data,options)
+    }
+    });
         return(
-            <div className="updates mdl-card">
-            <QuestionCard question={this.state.results[this.state.iter]} />
+            <div className="updates chart mdl-card">
             </div>
         );
-    }
-    else{
-        return(
-        <div></div>
-        );
-    }
     },
-});
-
-var QuestionCard = React.createClass({
-
-    render:function(){
-        if(this.props.question.response_format == "rating"){
-            var data = {
-                series : this.props.question.bar_data.series,
-                labels : this.props.question.bar_data.labels
-            };
-            var options= {
-                seriesBarDistance: 15
-            };
-            new Chartist.Bar('.chart',data, options)
-        }
-        //pie data is represented as the set of options chosen with a value indicating
-        //how many times that set is chosen.
-        else if(this.props.question.response_format == "trueOrFalse"){
-
-        }
-        else if(this.props.question.response_format == "multipleChoice"){
-
-        }
-        else if(this.props.question.response_format == "freeResponse"){
-
-        }
-        
-        return(
-        <div className="chart">
-        </div>
-        );
-    }
 });
