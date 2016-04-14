e68b6aca39460fe5ff2b37d5eef8d06f051eb190
Bradyta <brady.auen@colorado.edu>
2016-04-14 00:19:32 -0600

    remove import

--- handlers/api/survey_results_handler.py
+++ handlers/api/survey_results_handler.py
@@ -3,7 +3,6 @@ import tornado.gen as gen
 import json
 import time
 import logging
-import numpy as np
 from time import sleep
 from models.survey import Survey
 from models.user import User
@@ -18,10 +17,4 @@ class SurveyResultsHandler(BaseHandler):
         gets the formatted results to a survey_id
         """
         results = Survey().get_formatted_results(survey_id)
-
-        #for i in results:
-        #    if i['response_format'] == 'multipleChoice':
-        #        i['bar_data']['series'] = np.sum(i['results'], axis=0).tolist()
-                
-        logging.info(results)
         return self.write(tornado.escape.json_encode(results))
